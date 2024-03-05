cdbdec461aa4a2a347a127c40ab4ca23377ceca0
quentinformatique <quentin161204@gmail.com>
2024-03-05 23:00:35 +0100

    remove pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import {library} from "@fortawesome/fontawesome-svg-core";
@@ -7,16 +6,11 @@ import {fas} from "@fortawesome/free-solid-svg-icons";
 
 library.add(fas)
 
-createApp(App)
-    .use(router)
-    .component('font-awesome-icon', FontAwesomeIcon)
-    .mount('#app')
-
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia()) //use pinia
 app.use(router) //use router
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
