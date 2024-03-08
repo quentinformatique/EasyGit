da3045293cb8ffee01cc90df79647e7048f18f75
quentinformatique <quentin161204@gmail.com>
2024-03-08 10:29:18 +0100

    remove all

--- src/electron/preload/preload.ts
+++
@@ -1,7 +0,0 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-import { contextBridge, ipcRenderer } from 'electron'
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  openFile: () => ipcRenderer.invoke('dialog:openFile')
-})
