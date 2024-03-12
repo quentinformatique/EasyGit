59a615249ada33426b53c046f04c45c2fd3c6e6a
quentinformatique <quentin161204@gmail.com>
2024-03-12 09:33:11 +0100

    Update App.vue

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,32 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
 
 window.electronAPI.sendMessage('Hello from App.vue!');
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <Header current-repo="test" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style lang="scss" scoped>
+
+.container {
+  height: 100vh;
+  display: flex;
+  background-color: #282c34;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+
 </style>
