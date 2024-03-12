25c2dd7f557e29bdc01ee57ea2ec504a3d8d4868
quentinformatique <quentin161204@gmail.com>
2024-03-12 09:54:28 +0100

    Create ChangesSideBar.vue

---
+++ src/renderer/components/SideBars/ChangesSideBar.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="left-side-bar">
+    <div class="item">GAUCHE</div>
+    <button>Commit</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+.left-side-bar {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 20vw;
+  background-color: #282c34;
+  box-shadow: 0 4px 7px 0 rgba(0, 0, 0, 0.2);
+  height: 100vh;
+
+  .item {
+    color: white;
+  }
+
+  button {
+    background-color: #99E1D9;
+    color: #282c34;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    margin: 20px;
+    width: 100px;
+    cursor: pointer;
+    transition: 0.3s;
+
+    &:hover {
+      transform: scale(1.1);
+      background-color: #7ED8D3;
+    }
+  }
+}
+
+</style>
