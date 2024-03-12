bb14c4b872a684697a844da0c278f99221471619
quentinformatique <quentin161204@gmail.com>
2024-03-12 09:33:09 +0100

    Update HelloWorld.vue

--- src/renderer/components/HelloWorld.vue
+++ src/renderer/components/HelloWorld.vue
@@ -10,24 +10,10 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
 </style>
