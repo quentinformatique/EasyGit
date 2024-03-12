e469caf1ec13eb5b9e5e9ff2b8be4dd5aaed8ac1
quentinformatique <quentin161204@gmail.com>
2024-03-12 09:32:49 +0100

    Create Header.vue

---
+++ src/renderer/components/Header.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+defineProps({
+      currentRepo: String
+    }
+)
+
+</script>
+
+<template>
+  <div class="header">
+    <div class="item">GAUCHE</div>
+    <div class="item">{{ currentRepo }}</div>
+    <div class="item">DROITE</div>
+  </div>
+</template>
+
+<style scoped>
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  width: 100vw;
+  background-color: #282c34;
+  box-shadow: 0 4px 7px 0 rgba(0, 0, 0, 0.2);
+  height: 50px;
+  .item {
+    color: white;
+  }
+}
+</style>
