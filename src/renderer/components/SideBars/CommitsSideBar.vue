f07a01e0e447fbee3465aa46b6e685e43429c843
quentinformatique <quentin161204@gmail.com>
2024-03-14 22:21:26 +0100

    Update CommitsSideBar.vue

--- src/renderer/components/SideBars/CommitsSideBar.vue
+++ src/renderer/components/SideBars/CommitsSideBar.vue
@@ -7,6 +7,9 @@
     <div class="top">
       <h2>History</h2>
     </div>
+    <div class="main">
+      commits
+    </div>
   </div>
 
 </template>
@@ -21,6 +24,7 @@
   color: white;
   box-sizing: border-box;
 
+  border-right: 1px solid #232323;
   display: flex;
   flex-direction: column;
 
@@ -31,6 +35,15 @@
     height: 80px;
     border-bottom: 1px solid #232323;
     border-left: 1px solid #232323;
+  }
+
+  .main {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    border-left: 1px solid #232323;
+    flex: 1;
   }
 }
 
