52c9c3fcf0f8622aa840a2854880e872110fcd88
quentinformatique <quentin161204@gmail.com>
2024-03-14 21:21:14 +0100

    add fa

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
-import './style.css';
-import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import App from "./App.vue";
 
-const app = createApp(App);
+library.add(fas)
 
-app.mount('#app');
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
