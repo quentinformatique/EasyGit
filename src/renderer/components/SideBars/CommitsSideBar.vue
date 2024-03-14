d4bada54631dc12d650198506d1ac33c2fac73e7
quentinformatique <quentin161204@gmail.com>
2024-03-14 21:21:05 +0100

    rename component

---
+++ src/renderer/components/SideBars/CommitsSideBar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="top">
+      <h2>History</h2>
+    </div>
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+.container {
+  width: 320px;
+  height: 800px;
+  background-color: #2C303A;
+  color: white;
+
+  display: flex;
+  flex-direction: column;
+
+  .top {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80px;
+    border-bottom: 1px solid #232323;
+    border-left: 1px solid #232323;
+  }
+}
+
+
+</style>
