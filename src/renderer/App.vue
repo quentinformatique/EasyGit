5390be080ea19e79dffe100e9b631e850417c310
quentinformatique <quentin161204@gmail.com>
2024-03-14 21:21:37 +0100

    Update App.vue

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,25 +1,63 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
 import LeftSideBar from "./components/SideBars/ChangesSideBar.vue";
+import CommitsSideBar from "./components/SideBars/CommitsSideBar.vue";
+import GestionSIdeBar from "./components/SideBars/GestionSIdeBar.vue";
 
 window.electronAPI.sendMessage('Hello from App.vue!');
 </script>
 
 <template>
-  <div class="container">
-    <Header current-repo="test" />
-    <LeftSideBar />
+  <div class="app-container">
+    <div class="left-section">
+      <Header currentRepo="Repo"/>
+      <div class="content-section">
+        <div class="sidebar-section">
+          <GestionSIdeBar/>
+          <LeftSideBar/>
+        </div>
+        <div class="main-section">
+          <div class="main-content">
+            main
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="right-section">
+      <CommitsSideBar/>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
-.container {
-  height: 100vh;
+.app-container {
   display: flex;
-  flex-direction: column;
-  background-color: #282c34;
+  background-color: white;
+
+  .left-section {
+    display: flex;
+    flex-direction: column;
+    .content-section {
+      display: flex;
+      flex: 1;
+    }
+
+    .sidebar-section {
+      display: flex;
+    }
+
+    .main-section {
+      .main-content {
+        flex: 1;
+        width: 550px;
+        height: 100%;
+        background-color: #282C34;
+      }
+    }
+  }
+
+  .right-section {
+    display: flex;
+  }
 }
-
-
 </style>
