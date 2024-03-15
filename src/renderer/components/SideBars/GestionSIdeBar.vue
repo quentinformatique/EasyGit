b897f776d08074848e13c4f852ac7df6f4380e0d
quentinformatique <quentin161204@gmail.com>
2024-03-15 22:45:40 +0100

    Update GestionSIdeBar.vue

--- src/renderer/components/SideBars/GestionSIdeBar.vue
+++ src/renderer/components/SideBars/GestionSIdeBar.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 
+import Action from "../buttons/Action.vue";
 </script>
 
 <template>
 
   <div class="container">
-    <div class="fisrt-actions">
-      <div class="action">push</div>
-      <div class="action">pull</div>
-      <div class="action">fork</div>
+    <div class="first-actions">
+      <Action bgColor="#6BC0BC" icon="upload"/>
+      <Action bgColor="#3F4552" icon="arrows-rotate"/>
+      <Action bgColor="#3F4552" icon="code-branch"/>
     </div>
-    <div class="second-actions">settings</div>
+    <div class="second-actions">
+      <Action bgColor="#3F4552" icon="gear"/>
+    </div>
   </div>
-
 </template>
 
 <style lang="scss" scoped>
@@ -22,10 +24,24 @@
   height: 100%;
   background-color: #2C303A;
   border-right: 1px solid #232323;
+  box-sizing: border-box;
 
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  align-items: center;
+
+  .first-actions {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    margin: 20px 0;
+    gap: 20px;
+  }
+  .second-actions {
+    margin-bottom: 20px;
+  }
 }
 
 </style>
