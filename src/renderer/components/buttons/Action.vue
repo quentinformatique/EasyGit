c0c4f9e0b0c9f0fdbbbeed08be182e943fdec858
quentinformatique <quentin161204@gmail.com>
2024-03-15 22:45:31 +0100

    Create Action.vue

---
+++ src/renderer/components/buttons/Action.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+defineProps({
+  bgColor: String,
+  icon: String
+})
+
+</script>
+
+<template>
+
+  <div class="button" :style="{ backgroundColor: bgColor }">
+    <font-awesome-icon :icon="icon"/>
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+
+.button {
+  width: 70px;
+  height: 70px;
+  color: white;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: 0.3s;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  .svg-inline--fa {
+    font-size: 35px;
+  }
+}
+
+</style>
