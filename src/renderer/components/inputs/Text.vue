cb66f08b649164d0d1e392fe15b65941b5a1a5f0
quentinformatique <quentin161204@gmail.com>
2024-03-18 15:09:58 +0100

    Create Text.vue

---
+++ src/renderer/components/inputs/Text.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+defineProps({
+    name: String,
+    id: String,
+    placeholder: String
+});
+
+</script>
+
+<template>
+  <input type="text" :name="name" :id="id" :placeholder="placeholder"/>
+</template>
+
+<style scoped lang="scss">
+
+
+input {
+  border: none;
+  background-color: #2C303A;
+  height: 30px;
+  color: white;
+  width: 90%;
+  border-radius: 5px;
+
+  &:focus {
+    outline: none;
+  }
+}
+
+</style>
