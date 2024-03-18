0eeefef29436908e31f7516573079be34721a677
quentinformatique <quentin161204@gmail.com>
2024-03-18 15:10:00 +0100

    Create Textarea.vue

---
+++ src/renderer/components/inputs/Textarea.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps({
+  name: String,
+  id: String,
+  placeholder: String,
+  rows: Number
+});
+</script>
+
+<template>
+  <textarea :name="name" :id="id" :placeholder="placeholder" :rows="rows"></textarea>
+</template>
+
+<style scoped lang="scss">
+
+textarea {
+  border: none;
+  background-color: #2C303A;
+  color: white;
+  border-radius: 5px;
+  width: 90%;
+  resize: none;
+
+  &:focus {
+    outline: none;
+  }
+}
+
+</style>
