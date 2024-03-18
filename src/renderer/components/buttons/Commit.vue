41643d8513dbd399b18e29e4179ad1fe3801a6ce
quentinformatique <quentin161204@gmail.com>
2024-03-18 15:09:55 +0100

    Create Commit.vue

---
+++ src/renderer/components/buttons/Commit.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+defineProps({
+  text: String
+})
+
+</script>
+
+<template>
+  <div class="commit-submit">
+    <button>{{ text }}</button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.commit-submit {
+  width: 100%;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  button {
+    width: 90%;
+    height: 100%;
+    border: none;
+    background-color: #6BC0BC;
+    color: white;
+    cursor: pointer;
+    transition: 0.3s;
+    border-radius: 5px;
+
+    &:hover {
+      background-color: #5BA9A2;
+    }
+  }
+}
+
+
+</style>
