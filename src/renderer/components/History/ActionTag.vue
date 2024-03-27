1e6ef7d7be4cd0c6e9223d5776995a084379c5e5
quentinformatique <quentin161204@gmail.com>
2024-03-27 21:05:54 +0100

    Create ActionTag.vue

---
+++ src/renderer/components/History/ActionTag.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps({
+  tag: String
+})
+</script>
+
+<template>
+
+  <div class="tag">
+    <div class="tag-content">
+      {{ tag }}
+    </div>
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+
+.tag {
+  background-color: #50555D;
+  color: white;
+  padding: 2px 5px;
+  border-radius: 2px;
+  height: 15px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .tag-content {
+    font-size: 12px;
+  }
+}
+
+</style>
