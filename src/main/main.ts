b3a6956f1ebb2edf64024da42933ecf8ff97e522
quentinformatique <quentin161204@gmail.com>
2024-04-14 21:23:39 +0100

    Update main.ts

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,53 +1,53 @@
 import {app, BrowserWindow, ipcMain, session} from 'electron';
 import {join} from 'path';
 
-function createWindow () {
-  const mainWindow = new BrowserWindow({
-    width: 1206,
-    height: 829,
-    resizable: false,
-    autoHideMenuBar: true,
-    webPreferences: {
-      preload: join(__dirname, 'preload.js'),
-      nodeIntegration: false,
-      contextIsolation: true,
-    }
-  });
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1206,
+        height: 829,
+        resizable: false,
+        autoHideMenuBar: true,
+        webPreferences: {
+            preload: join(__dirname, 'preload.js'),
+            nodeIntegration: false,
+            contextIsolation: true,
+        },
+    });
 
-  if (process.env.NODE_ENV === 'development') {
-    const rendererPort = process.argv[2];
-    mainWindow.loadURL(`http://localhost:${rendererPort}`);
-  }
-  else {
-    mainWindow.loadFile(join(app.getAppPath(), 'renderer', 'index.html'));
-  }
+
+    if (process.env.NODE_ENV === 'development') {
+        const rendererPort = process.argv[2];
+        mainWindow.loadURL(`http://localhost:${rendererPort}`);
+    } else {
+        mainWindow.loadFile(join(app.getAppPath(), 'renderer', 'index.html'));
+    }
 }
 
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 
-  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
-    callback({
-      responseHeaders: {
-        ...details.responseHeaders,
-        'Content-Security-Policy': ['script-src \'self\'']
-      }
+    session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+        callback({
+            responseHeaders: {
+                ...details.responseHeaders,
+                'Content-Security-Policy': ['script-src \'self\'']
+            }
+        })
     })
-  })
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
 });
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
 });
 
 ipcMain.on('message', (event, message) => {
-  console.log(message);
+    console.log(message);
 })
